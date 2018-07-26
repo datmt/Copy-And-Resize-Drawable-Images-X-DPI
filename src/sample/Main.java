25c2e1fe780d36e8695dadc637c0c19eb17dc854
myn <t2dx.inc@gmail.com>
2018-07-26 16:35:45 +0700

    add desc

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Copy and resize your images to x-dpi folders");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
