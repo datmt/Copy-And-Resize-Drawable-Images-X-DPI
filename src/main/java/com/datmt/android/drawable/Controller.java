6099a1ab5ebeef85883f53158eb219c81c14d13d
Mạnh Đạt <dat.tm24@gmail.com>
2023-10-30 09:10:47 +0700

    add label

--- src/main/java/com/datmt/android/drawable/Controller.java
+++ src/main/java/com/datmt/android/drawable/Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -32,6 +33,8 @@ public class Controller {
     private static final String PREF_NAME = "BC_PREFS_IMAGE_TO_XDPI";
     private static final String FOLDER = "BC_PREFS_IMAGE_TO_XDPI";
 
+    @FXML
+    Label copyImageStatusLB;
     @FXML
     Hyperlink bcLink;
 
@@ -61,17 +64,14 @@ public class Controller {
     public TextField folderPathTF;
 
     @FXML
-    public void initialize()
-    {
+    public void initialize() {
         imageView.setImage(new Image("https://binarycarpenter.com/wp-content/uploads/2018/09/bc-logo.png"));
         imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                try
-                {
+                try {
                     Desktop.getDesktop().browse(new URI("https://binarycarpenter.com/"));
-                } catch (Exception ex)
-                {
+                } catch (Exception ex) {
                     ex.printStackTrace();
                 }
 
@@ -108,31 +108,27 @@ public class Controller {
 
     }
 
-    private Preferences getPrefs()
-    {
+    private Preferences getPrefs() {
         if (prefs == null)
             prefs = Preferences.userRoot().node(this.getClass().getName());
 
         return prefs;
     }
 
-    public void selectDrawableFolder()
-    {
+    public void selectDrawableFolder() {
         DirectoryChooser chooser = new DirectoryChooser();
         chooser.setTitle("Select your /res folder");
 
         System.out.println(root.getScene() == null);
         File selectedFolder = chooser.showDialog(root.getScene().getWindow());
 
-        if (selectedFolder != null)
-        {
+        if (selectedFolder != null) {
             getPrefs().put(FOLDER, selectedFolder.getAbsolutePath());
             folderPathTF.setText(selectedFolder.getAbsolutePath());
         }
     }
 
-    public void selectImageFile()
-    {
+    public void selectImageFile() {
         FileChooser imageChooser = new FileChooser();
 
         imageChooser.setTitle("Select your image");
@@ -140,48 +136,51 @@ public class Controller {
                 new FileChooser.ExtensionFilter("PNG", "*.png"),
                 new FileChooser.ExtensionFilter("JPG", "*.jpg")
 
-                );
+        );
 
         imageFiles = imageChooser.showOpenMultipleDialog(root.getScene().getWindow());
 
     }
 
     public void copyImages() {
-        if (imageFiles == null || imageFiles.size() == 0)
+        if (imageFiles == null || imageFiles.size() == 0) {
+            copyImageStatusLB.setText("Please select at least one image file");
             return;
+        }
 
+        copyImageStatusLB.setText("Copying...");
 
-        for (File singleImage : imageFiles)
-        {
-            try
-            {
+        for (File singleImage : imageFiles) {
+            try {
                 BufferedImage originalImage = ImageIO.read(singleImage);
                 int type = BufferedImage.TYPE_INT_ARGB;
                 String imageName = singleImage.getName();
                 String imageExtension = singleImage.getName().substring(singleImage.getName().length() - 3);
 
                 Preferences prefs = getPrefs();
-
+                var resFolder = prefs.get(FOLDER, folderPathTF.getText());
+                if (resFolder.endsWith("/"))
+                    resFolder = resFolder.substring(0, resFolder.length() - 1);
                 if (xxhdpiCB.isSelected())
-                    ImageIO.write(resizeImage(originalImage, type, 144, 144), imageExtension, new File(prefs.get(FOLDER, "") + "/drawable-xxhdpi/" + imageName)  );
+                    ImageIO.write(resizeImage(originalImage, type, 144, 144), imageExtension, new File(resFolder + "/drawable-xxhdpi/" + imageName));
                 if (xhdpiCB.isSelected())
-                    ImageIO.write(resizeImage(originalImage, type, 96, 96), imageExtension, new File(prefs.get(FOLDER, "") + "/drawable-xhdpi/" + imageName)  );
+                    ImageIO.write(resizeImage(originalImage, type, 96, 96), imageExtension, new File(resFolder + "/drawable-xhdpi/" + imageName));
                 if (hdpiCB.isSelected())
-                    ImageIO.write(resizeImage(originalImage, type, 72, 72), imageExtension, new File(prefs.get(FOLDER, "") + "/drawable-hdpi/" + imageName)  );
+                    ImageIO.write(resizeImage(originalImage, type, 72, 72), imageExtension, new File(resFolder + "/drawable-hdpi/" + imageName));
                 if (mdpiCB.isSelected())
-                    ImageIO.write(resizeImage(originalImage, type, 48, 48), imageExtension, new File(prefs.get(FOLDER, "") + "/drawable-mdpi/" + imageName)  );
+                    ImageIO.write(resizeImage(originalImage, type, 48, 48), imageExtension, new File(resFolder + "/drawable-mdpi/" + imageName));
 
 
-
-            } catch (IOException ex)
-            {
+            } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
 
+        copyImageStatusLB.setText("Done");
+
     }
 
-    private static BufferedImage resizeImage(BufferedImage originalImage, int type, int width, int height){
+    private static BufferedImage resizeImage(BufferedImage originalImage, int type, int width, int height) {
 
         BufferedImage resizedImage = new BufferedImage(width, height, type);
         Graphics2D g = resizedImage.createGraphics();
